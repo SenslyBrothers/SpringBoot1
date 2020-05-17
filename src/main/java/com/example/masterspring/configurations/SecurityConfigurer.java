010095fb3ac96c7295ca378c78f02479d9ed0e0a
udulaindunil <udulaindunil@gmail.com>
2020-05-17 23:42:34 +0530

    Jwt taken Generation and the jwt javabrain vedio 1 done

--- src/main/java/com/example/masterspring/configurations/SecurityConfigurer.java
+++ src/main/java/com/example/masterspring/configurations/SecurityConfigurer.java
@@ -7,9 +7,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.masterspring.filters.JwtRequestFilter;
 import com.example.masterspring.service.MyUserDetailsService;
 
 @EnableWebSecurity
@@ -17,6 +20,9 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private MyUserDetailsService myUserDetailService;
+	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -33,7 +39,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable()
-		.authorizeRequests().antMatchers("/authenticate").permitAll();
+		.authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		
 	}
 	
 	@Override
